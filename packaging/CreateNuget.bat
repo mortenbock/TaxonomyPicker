aaefb8f5dba7921983a1f085e988198139efcd4f
mortenbock <morten@mortenbock.dk>
2015-02-18 14:53:16 +0100

    Arrange build artifacts, and ignore output

--- packaging/CreateNuget.bat
+++ packaging/CreateNuget.bat
@@ -1,2 +1,2 @@
-if not exist NuGetRelease mkdir NuGetRelease
-..\tools\NuGet\NuGet.exe pack ..\src\TaxonomyPicker\TaxonomyPicker.csproj -Build -Properties Configuration=Release -OutputDirectory NuGetRelease -Verbosity detailed
+if not exist Release mkdir Release
+..\tools\NuGet\NuGet.exe pack ..\src\TaxonomyPicker\TaxonomyPicker.csproj -Build -Properties Configuration=Release -OutputDirectory Release -Verbosity detailed
--- packaging/CreateUmbracoPackage.ps1
+++ packaging/CreateUmbracoPackage.ps1
@@ -1,39 +1,4 @@
-﻿$packageGuid = [guid]::NewGuid().ToString("D")
-$timeStamp = (Get-Date -Format yyyy-MM-dd_hh-mm-ss)
-$currentFolder = (Get-Item $MyInvocation.MyCommand.Definition).Directory
-$manifestFile = (Join-Path $currentFolder "package.xml")
-$dllFile = (Join-Path $currentFolder.Parent.FullName "src\TaxonomyPicker\bin\Release\TaxonomyPicker.dll")
-$packFolderParent = (Join-Path $currentFolder ("UmbracoRelease/" + $timeStamp))
-$packFolder = (Join-Path $packFolderParent $packageGuid)
-
-#Check requisites
-if(!(Test-Path $dllFile)) 
-{ 
-    Write-Host "TaxonomyPicker.dll not found." -ForegroundColor Red
-    Write-Host $dllFile -ForegroundColor Red
-    Exit
-}
-
-$dllVersion = (get-assembly-version $dllFile)
-$outFile = (Join-Path $currentFolder ("TaxonomyPicker_" + $dllVersion + ".zip"))
-
-#Create tmp folder
-New-Item $packFolder -ItemType directory
-
-#Copy manifest file
-$manifestXml = [xml](Get-Content $manifestFile)
-$manifestXml.umbPackage.info.package.version = $dllVersion
-$manifestOutput = (Join-Path $packFolder "package.xml")
-$manifestXml.Save($manifestOutput)
-
-#Copy dll
-Copy-Item $dllFile $packFolder 
-
-#Zip folder as package
-ZipFiles $outFile $packFolderParent
-
-
-function ZipFiles($zipfilename, $sourcedir)
+﻿function ZipFiles($zipfilename, $sourcedir)
 {
     Add-Type -Assembly System.IO.Compression.FileSystem
     $compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
@@ -48,3 +13,38 @@ function get-assembly-version() {
     #format the version and output it...
     $version.ToString()
 }
+
+$packageGuid = [guid]::NewGuid().ToString("D")
+$currentFolder = (Get-Item $MyInvocation.MyCommand.Definition).Directory
+$manifestFile = (Join-Path $currentFolder "package.xml")
+$dllFile = (Join-Path $currentFolder.Parent.FullName "src\TaxonomyPicker\bin\Release\TaxonomyPicker.dll")
+$releaseFolder = (Join-Path $currentFolder "Release")
+$packFolder = (Join-Path $releaseFolder $packageGuid)
+
+#Check requisites
+if(!(Test-Path $dllFile)) 
+{ 
+    Write-Host "TaxonomyPicker.dll not found." -ForegroundColor Red
+    Write-Host $dllFile -ForegroundColor Red
+    Exit
+}
+
+$dllVersion = (get-assembly-version $dllFile)
+$outFile = (Join-Path $releaseFolder ("TaxonomyPicker_" + $dllVersion + ".zip"))
+
+#Create tmp folder
+New-Item $packFolder -ItemType directory
+
+#Copy manifest file
+$manifestXml = [xml](Get-Content $manifestFile)
+$manifestXml.umbPackage.info.package.version = $dllVersion
+$manifestOutput = (Join-Path $packFolder "package.xml")
+$manifestXml.Save($manifestOutput)
+
+#Copy dll
+Copy-Item $dllFile $packFolder 
+
+#Zip folder as package
+ZipFiles $outFile $packFolder
+
+
